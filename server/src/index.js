709a83c26f2c4dc54b1fa939aeafd5a1641175e2
lmilanov <lubaka05@gmail.com>
2025-03-25 11:10:14 +0200

    added DB connection and setup cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,38 @@
 import express from 'express';
+import mongoose from 'mongoose'
+
+
+
+
+try{
+    const uri = 'mongodb://localhost:27017/cameras';
+    await mongoose.connect(uri);
+    console.log('Successfuly connected to DB!')
+}catch(err){
+    console.log('Connection to DB failed!');
+    console.log(err.message);
+}
+
+
+// setup CORS
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', "*");
+})
+
 
 
 const app = express();
 
 
 app.get('/', (req,res)=>{
-    res.send('it works')
+    res.json({message:'It works!'})
 });
+
+
+app.get('/data/catalog', (req,req)=>{
+    res.jason({message: 'THIS IS THE CATALOG'})
+})
 
 
 app.listen(3030,()=>console.log('restful server running on http://localhost:3030...'))

f9482a763199db90f0d7f45dc50a7635a6acee6c
Lepp3 <lubaka05@gmail.com>
2025-03-26 21:12:06 +0200

    added primary routes without component elements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,18 @@ function App() {
   return (
     <>
     <Header/>
-    
+    <main id="main-content">
+    <Routes>
+      <Route path='/' />
+      <Route path='/login'/>
+      <Route path='/register'/>
+      <Route path='/logout'/>
+      <Route path='/cameras' />
+      <Route path='/cameras/:cameraId/details'/>
+      <Route path='/cameras/:cameraId/edit'/>
+      <Route path='/cameras/create'/>
+    </Routes>
+    </main>
     </>
   )
 }

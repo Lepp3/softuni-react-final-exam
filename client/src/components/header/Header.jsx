e4dd867e3b9931eb900faec02cc4b899a0c71c89
lmilanov <lubaka05@gmail.com>
2025-03-29 13:33:55 +0200

    added dynamic navigation rendering based on login state

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react"
 import { Link } from "react-router"
+import { UserContext } from "../../contexts/UserContext"
 
 export default function Header(){
     //TODO AUTHENTICATION FOR DYNAMIC RENDER OF BUTTONS
+
+    const { email, userId} = useContext(UserContext);
     return(
         <header>
             <h1><Link to="/">CameraWorld</Link></h1>
@@ -9,15 +13,17 @@ export default function Header(){
             <Link to="/">Home</Link>
             <Link to="/cameras">Cameras</Link>
             <Link to="/about">About</Link>
-            <div id="guest">
+            {email ?
+             <div id="user">
+             <Link to="/cameras/create">Post a camera</Link>
+             <Link to="/logout">Logout</Link>
+             <Link to={`/user/${userId}`}>{email}'s Profile</Link>
+             </div>
+            : <div id="guest">
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
             </div>
-            <div id="user">
-            <Link to="/cameras/create">Post a camera</Link>
-            <Link to="/logout">Logout</Link>
-            <Link to="/user/userId">Profile Page</Link>
-            </div>
+            }
             </nav>
         </header>
     )

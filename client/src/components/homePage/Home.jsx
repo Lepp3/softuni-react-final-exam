edd493438943d13a8bcc343e3bd67468d50f4f24
lmilanov <lubaka05@gmail.com>
2025-04-01 15:13:35 +0300

    added dynamic action buttons on home page depending on auth state

--- client/src/components/homePage/Home.jsx
+++ client/src/components/homePage/Home.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router"
+import useAuth from "../../hooks/useAuth"
+
 export default function Home(){
+
+    const {isAuthenticated} = useAuth()
+    
     return(
         <section>
-            <h1>This is where the homepage content will be</h1>
+            <h1>Welcome to Camera World!</h1>
+            <div id="actionButtons">
+                {isAuthenticated ? <Link to="/cameras/create"> Post a camera!</Link> : <Link to="/login"> Log in to start posting!</Link>}
+            </div>
         </section>
     )
 }
